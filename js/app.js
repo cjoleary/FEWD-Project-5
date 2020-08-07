77d3b9546084a5fb3f2758ca66110d5abfc960f9
cjoleary <cjoleary17@gmail.com>
2020-08-07 17:30:15 -0400

    added search filter function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
 
 baguetteBox.run('.gallery');
 
-const search = new Filter('search', 'data-caption');
+// const search = new Filter('search', 'data-caption');
 
+// create a function
+$(document).ready( function() {
+    // declare a variable that will get my images
+    let images = document.getElementsByTagName('a');
+        // use an event that will keep track of my keystrokes
+        $('#search').on( 'keyup', function() {
+        // tell it what you want to happen when you type
+        let search = $('#search').val().toLowerCase();
+        // loop through all of my images
+        for ( var i = 0; i < images.length; i++ ){
+            // look for a particular attribute on each image
+            let searchVal = images[i].getAttribute('data-caption');
+            // if an image has that attribute, show it
+            if ( searchVal.toLowerCase().indexOf(search) > -1 ) {
+                images[i].style.display = '';
+            } 
+            // if not, do not show it
+            else {
+                images[i].style.display = 'none';
+            }
+        }
+    });
+});
