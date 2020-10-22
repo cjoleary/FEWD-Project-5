aa5bd5a1a2928432549a7c731f1ee46789d22d05
cjoleary <cjoleary17@gmail.com>
2020-10-22 17:31:10 -0600

    replaced HTML with a for loop to dynamically generate the HTML for the photo gallery

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
 
-baguetteBox.run('.gallery');
-
-// const search = new Filter('search', 'data-caption');
-
-// create a function
+// search filter functionality
 $(document).ready( function() {
     // declare a variable that will get my images
     let images = document.getElementsByTagName('a');
@@ -27,3 +23,49 @@ $(document).ready( function() {
     });
 });
 
+// generate HTML photo gallery
+
+const dataCaption = [
+    'I love hay bales. Took this snap on a drive through the countryside past some straw fields.',
+    'The lake was so calm today. We had a great view of the snow on the mountains from here.',
+    'I hiked to the top of the mountain and got this picture of the canyon and trees below.',
+    'It was amazing to see an iceberg up close, it was so cold but didn’t snow today. ',
+    'The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.',
+    'Fall is coming, I love when the leaves on the trees start to change color.',
+    'I drove past this plantation yesterday, everything is so green!',
+    'My summer vacation to the Oregon Coast. I love the sandy dunes!',
+    'We enjoyed a quiet stroll down this countryside lane.',
+    'Sunset at the coast! The sky turned a lovely shade of orange.',
+    'I did a tour of a cave today and the view of the landscape below was breathtaking.',
+    'I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in.'
+]
+
+const alt = [
+    'Hay Bales',
+    'Lake',
+    'Canyon',
+    'Iceberg',
+    'Desert',
+    'Fall',
+    'Plantation',
+    'Dunes',
+    'Countryside Lane',
+    'Sunset',
+    'Cave',
+    'Bluebells'
+]
+
+const gallery = document.querySelector('#gallery');
+
+for ( let i = 0; i < alt.length; i++ ) {
+    gallery.innerHTML += `
+        <div class="box">
+            <a href="photos/${[i+1]}.jpg" data-caption="${dataCaption[i]}">
+                <img src="photos/thumbnails/${[i+1]}.jpg" alt="${alt[i]}">
+            </a>
+        </div>
+    `;
+}
+
+// light box plugin for photo gallery
+baguetteBox.run('.gallery');
